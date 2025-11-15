2238a3dfee550ee189baf2be0c5cbd271b5b7f01
naveenkarthi007 <naveenkarthi.mn@gmail.com>
2025-11-15 12:18:00 +0530

    deplpoy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/YT_Clone">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
